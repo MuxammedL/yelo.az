481f0608f099f049e59094d129fe96bf7d6454aa
MuxammedL <layicovmuxammed@gmail.com>
2023-11-10 22:20:24 +0400

    update stories

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -8,32 +8,85 @@ var entranceSwiper = new Swiper(".entranceSwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
-}); //1.15 375 350 1.08
-
-function calculateSlidesPerView() {
-  if (window.innerWidth < 375) {
-    return 1;
-  } else if (window.innerWidth < 480) {
-    return 1.15;
-  } else if (window.innerWidth < 768) {
-    return 1.4;
-  } else if (window.innerWidth < 992) {
-    return 2.4;
-  } else if (window.innerWidth < 1200) {
-    return 2.85;
-  } else if (window.innerWidth < 1400) {
-    return 2.95;
-  } else {
-    return 2.9;
-  }
-}
-
-var swiperStories = new Swiper(".stories", {
-  slidesPerView: calculateSlidesPerView(),
 });
 
-window.addEventListener("resize", () => {
-  var swiperStories = new Swiper(".stories", {
-    slidesPerView: calculateSlidesPerView(),
+var swiperStories = new Swiper(".stories", {
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    375: {
+      slidesPerView: 1.15,
+    },
+    425: {
+      slidesPerView: 1.3,
+    },
+    480: {
+      slidesPerView: 1.5,
+    },
+    576: {
+      slidesPerView: 1.8,
+    },
+    768: {
+      slidesPerView: 2.4,
+    },
+    1024: {
+      slidesPerView: 2.65,
+    },
+    1200: {
+      slidesPerView: 2.85,
+    },
+    1400: {
+      slidesPerView: 2.9,
+    },
+  },
+});
+
+function fillBars() {
+  const item = document.querySelector(".swiper-slide-active");
+  const holder0 = document.querySelector(".index-0");
+  const label = item.getAttribute("aria-label");
+  switch (label) {
+    case "1 / 4":
+      holder0.style.animation = `fillBar 10.2s linear infinite`;
+      break;
+    case "3 / 3":
+      holder0.style.animation = `fillBar 10.2s linear infinite`;
+      break;
+    case '1 / 3':
+      const holder1 = document.querySelector(".index-1");
+      holder1.style.animation = `fillBar 10.2s linear infinite`;
+      break;
+    case '2 / 3':
+      const holder2 = document.querySelector(".index-2");
+      holder2.style.animation = `fillBar 10.2s linear infinite`;
+      break;
+  }
+}
+var swiper = new Swiper(".storiesSwiper", {
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 10000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-stories-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  on: fillBars(),
+});
+
+swiper.on("slideChange", () => {
+  const holders = document.querySelectorAll(".bar .holder");
+  const item = document.querySelector(".swiper-slide-active");
+  holders.forEach((hold) => {
+    hold.style.width = "0";
+    hold.style.animation = ``;
   });
+  fillBars();
 });
