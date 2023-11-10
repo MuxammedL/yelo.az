85daadb2ca74532860b55d59143286070fbee080
MuxammedL <layicovmuxammed@gmail.com>
2023-11-10 14:59:46 +0400

    added stories

--- src/js/main.js
+++ src/js/main.js
@@ -1,82 +1,91 @@
 const closeBtnPromotion = document.querySelector(".close-btn");
 const mobileTop = document.querySelector(".mobile-top");
-const header = document.querySelector('header');
-const selectBox = document.querySelector('.select-box')
-const selectedLang = document.querySelector('.selected-lang')
-const languages = selectBox.querySelectorAll('li')
-const searchBtn = document.querySelector('.search-btn')
-const closeSearchBtn = document.querySelector('.close-btn-search')
-const searchSection = document.querySelector('.search-section')
-const menuToggle = document.querySelector('.menu-toggle')
-const body = document.querySelector('body')
-const headerAdditions = document.querySelector('.header-additions')
-const navbar = document.querySelector('.navbar')
-const bgColor = document.querySelector('.bg-color')
+const header = document.querySelector("header");
+const selectBox = document.querySelector(".select-box");
+const selectedLang = document.querySelector(".selected-lang");
+const languages = selectBox.querySelectorAll("li");
+const searchBtn = document.querySelector(".search-btn");
+const closeSearchBtn = document.querySelector(".close-btn-search");
+const searchSection = document.querySelector(".search-section");
+const menuToggle = document.querySelector(".menu-toggle");
+const body = document.querySelector("body");
+const headerAdditions = document.querySelector(".header-additions");
+const navbar = document.querySelector(".navbar");
+const bgColor = document.querySelector(".bg-color");
+const main = document.querySelector("main");
 let prevScrollY = window.scrollY;
 
-
-
-
-window.addEventListener('scroll',()=>{
+window.addEventListener("scroll", () => {
   let currentScrollY = window.scrollY;
-  if(currentScrollY>prevScrollY && currentScrollY>10){
-    header.classList.add('fix-head')
-  }else{
-    header.classList.remove('fix-head')
+  if (currentScrollY > prevScrollY && currentScrollY > 10) {
+    header.classList.add("fix-head");
+  } else {
+    header.classList.remove("fix-head");
   }
-  if(window.scrollY>10){
-    header.classList.add('add-bg')
-  }else{
-    header.classList.remove('add-bg')
+  if (window.scrollY > 10) {
+    header.classList.add("add-bg");
+  } else {
+    header.classList.remove("add-bg");
   }
-  prevScrollY = currentScrollY
-})
+  prevScrollY = currentScrollY;
+});
 
-menuToggle.addEventListener('click',()=>{
-  menuToggle.classList.toggle('open-adds')
-  body.classList.toggle('o-hidden')
-  headerAdditions.classList.toggle('show')
-  navbar.classList.toggle('hide')
-  bgColor.classList.toggle('show')
-})
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("open-adds");
+  body.classList.toggle("o-hidden");
+  headerAdditions.classList.toggle("show");
+  navbar.classList.toggle("hide");
+  bgColor.classList.toggle("show");
+});
 
-searchBtn.addEventListener('click',()=>{
-  searchSection.classList.add('show')
-})
-closeSearchBtn.addEventListener('click',()=>{
-  searchSection.classList.remove('show')
-})
+searchBtn.addEventListener("click", () => {
+  searchSection.classList.add("show");
+});
+closeSearchBtn.addEventListener("click", () => {
+  searchSection.classList.remove("show");
+});
 
-
-window.addEventListener('resize',()=>{
-  if(window.innerWidth>768){
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 768) {
     if (sessionStorage.getItem("status") != "hide") {
       mobileTop.classList.add("show");
-      header.style.paddingTop = '120px'
+      header.style.paddingTop = "120px";
+      main.style.paddingTop = "120px";
     }
+  } else {
+    mobileTop.classList.remove("show");
+    header.style.paddingTop = "0px";
+    main.style.paddingTop = "0px";
   }
-})
+});
+
+if (sessionStorage.getItem("status") != "hide") {
+  mobileTop.classList.add("show");
+  header.style.paddingTop = "120px";
+  main.style.paddingTop = "120px";
+}
 
 closeBtnPromotion.addEventListener("click", () => {
-  mobileTop.style.height = "0px";
-  header.style.paddingTop = '0px'
+  mobileTop.classList.remove("show");
+  header.style.paddingTop = "0px";
+  main.style.paddingTop = "0px";
   sessionStorage.setItem("status", "hide");
 });
 
-selectedLang.addEventListener('click',(e)=>{
-  selectBox.classList.add('open-select')
-})
+selectedLang.addEventListener("click", (e) => {
+  selectBox.classList.add("open-select");
+});
 
-languages.forEach(lang=>{
-  lang.addEventListener('click',()=>{
-    selectBox.querySelector('li a.active').classList.remove('active');
-    lang.firstElementChild.classList.add('active')
+languages.forEach((lang) => {
+  lang.addEventListener("click", () => {
+    selectBox.querySelector("li a.active").classList.remove("active");
+    lang.firstElementChild.classList.add("active");
     selectedLang.textContent = lang.firstElementChild.textContent;
-  })
-})
+  });
+});
 
-window.addEventListener('click',(e)=>{
-  if(e.target != selectBox&&e.target != selectedLang){
-    selectBox.classList.remove('open-select')
+window.addEventListener("click", (e) => {
+  if (e.target != selectBox && e.target != selectedLang) {
+    selectBox.classList.remove("open-select");
   }
-})
+});
--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,11 +1,39 @@
-var swiper = new Swiper(".entranceSwiper", {
-    loop:true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+var entranceSwiper = new Swiper(".entranceSwiper", {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+}); //1.15 375 350 1.08
+
+function calculateSlidesPerView() {
+  if (window.innerWidth < 375) {
+    return 1;
+  } else if (window.innerWidth < 480) {
+    return 1.15;
+  } else if (window.innerWidth < 768) {
+    return 1.4;
+  } else if (window.innerWidth < 992) {
+    return 2.4;
+  } else if (window.innerWidth < 1200) {
+    return 2.85;
+  } else if (window.innerWidth < 1400) {
+    return 2.95;
+  } else {
+    return 2.9;
+  }
+}
+
+var swiperStories = new Swiper(".stories", {
+  slidesPerView: calculateSlidesPerView(),
+});
+
+window.addEventListener("resize", () => {
+  var swiperStories = new Swiper(".stories", {
+    slidesPerView: calculateSlidesPerView(),
+  });
 });
