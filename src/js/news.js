1cab81cc9121d3508fd85d6605a64aec977f1d45
MuxammedL <layicovmuxammed@gmail.com>
2023-11-19 14:55:30 +0400

    adding newsAdd method

--- src/js/news.js
+++ src/js/news.js
@@ -1,21 +1,23 @@
 const newsListTotal = document.querySelector(".news-list-total");
 const moreNews = document.getElementById("more-news");
-const textArea = document.querySelector('textarea')
+const textArea = document.querySelector("textarea");
 const loader = document.querySelector(".loader");
-const addNews = document.querySelector('.add-news')
-const newsModal = document.querySelector('.news-modal')
-const closeBtn = document.querySelector('.close-button')
+const addNews = document.querySelector(".add-news");
+const newsModal = document.querySelector(".news-modal");
+const closeBtn = document.querySelector(".close-button");
+const addBtn = document.querySelector(".add-button");
+const form = document.querySelector("#addNewsForm");
 let count = 15;
-let initialCount = 0,dataLength;
-
-
+let initialCount = 0,
+  dataLength;
+let isUpdate = false,
+  updatedId;
 async function getNews() {
   try {
     const res = await fetch("http://localhost:3000/NEWS");
     const data = await res.json();
-    dataLength=data.length;
-
-    for (let i = initialCount; i < count; i++) {
+    dataLength = data.length;
+    for (let i = dataLength - initialCount - 1; i >= dataLength - count; i--) {
       let date = new Date(data[i].date);
       months = [
         "Yanvar",
@@ -64,21 +66,39 @@ async function getNews() {
     }
     const deleteBtns = document.querySelectorAll(".delete");
     const editBtns = document.querySelectorAll(".edit");
-  
+
     deleteBtns.forEach((deleteBtn) => {
       deleteBtn.addEventListener("click", async (event) => {
         event.preventDefault();
         try {
-          const response = await fetch(`http://localhost:3000/NEWS/${deleteBtn.dataset.id}`, {
-            method: 'DELETE'
-          });
-  
-          
+          const response = await fetch(
+            `http://localhost:3000/NEWS/${deleteBtn.dataset.id}`,
+            {
+              method: "DELETE",
+            }
+          );
         } catch (error) {
-          console.error('Error deleting news:', error);
+          console.error("Error deleting news:", error);
         }
+        getNews();
       });
     });
+
+    editBtns.forEach((editBtn) => {
+      editBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        newsModal.classList.add("show");
+        body.classList.add("o-hidden");
+        newsModal.style.top = window.scrollY + "px";
+        let text =
+          editBtn.parentElement.parentElement.previousElementSibling.children[0]
+            .textContent;
+        textArea.value = text;
+        updatedId = editBtn.dataset.id;
+        isUpdate = true;
+      });
+    });
+
     loader.classList.add("hide");
     const showBtns = document.querySelectorAll(".dropdown button");
     showBtns.forEach((showBtn) => {
@@ -95,7 +115,6 @@ async function getNews() {
         }
       });
     });
-    
 
     window.addEventListener(
       "click",
@@ -118,34 +137,96 @@ async function getNews() {
   }
 }
 
-getNews()
-
+getNews();
 
 moreNews.addEventListener("click", () => {
   initialCount = count;
+  console.log(initialCount);
+  console.log(count);
   count += 15;
   if (count < dataLength) {
     loader.classList.remove("hide");
     getNews();
-  }else if(count-dataLength >0&&initialCount<dataLength){
-    count = count - 15 + dataLength%15
+  } else if (count - dataLength >= 0 && initialCount < dataLength) {
+    count = count + (dataLength % 15);
     getNews();
   }
-
-
+  console.log(count);
 });
 
-textArea.addEventListener('input',(e)=>{
-  textArea.style.height = `auto`
-  textArea.style.height = `${e.target.scrollHeight}px`
-})
+textArea.addEventListener("input", (e) => {
+  textArea.style.height = `auto`;
+  textArea.style.height = `${e.target.scrollHeight}px`;
+});
 
-addNews.addEventListener('click',()=>{
-  newsModal.classList.add('show')
-  body.classList.add('o-hidden')
-})
+addNews.addEventListener("click", () => {
+  textArea.value=''
+  newsModal.classList.add("show");
+  body.classList.add("o-hidden");
+  newsModal.style.top = window.scrollY + "px";
+});
 
-closeBtn.addEventListener('click',()=>{
-  newsModal.classList.remove('show')
-  body.classList.remove('o-hidden')
-})
+closeBtn.addEventListener("click", () => {
+  newsModal.classList.remove("show");
+  body.classList.remove("o-hidden");
+  newsModal.style.top = "0px";
+  isUpdate = false;
+});
+
+addNewsForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let isNotNull = false;
+  if (!isUpdate) {
+    const formData = {};
+    if (!textArea.value == "") {
+      formData[textArea.name] = textArea.value;
+      const currentDate = new Date();
+      const isoDate = currentDate.toISOString();
+      formData.date = isoDate;
+      isNotNull = true;
+    } else {
+      textArea.style.borderColor = "red";
+    }
+    if (isNotNull) {
+      fetch("http://localhost:3000/NEWS", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(formData),
+      });
+      setInterval(() => {
+        getNews();
+      }, 1000);
+      addNews.classList.remove("show");
+      body.classList.remove("o-hidden");
+      newsModal.style.top = "0px";
+    }
+  } else {
+    let data;
+    if (!textArea.value == "") {
+      data ={
+        title:`${textArea.value}`
+      }
+      isNotNull = true;
+    } else {
+      textArea.style.borderColor = "red";
+    }
+    if (isNotNull) {
+      fetch(`http://localhost:3000/NEWS/${updatedId}`, {
+        method: "PACTH",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(data),
+      });
+      setInterval(() => {
+        getNews();
+      }, 1000);
+      addNews.classList.remove("show");
+      body.classList.remove("o-hidden");
+      newsModal.style.top = "0px";
+    }
+  }
+  isUpdate = false;
+});
